4bf336f4eb4fa9448e1af0d2b75d021a50aed18d
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-03-26 19:07:46 +0100

    added Joi validation for incoming data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JoiPipeModule } from 'nestjs-joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [PokemonModule, MongooseModule.forRoot("mongodb+srv://Bianca999:vOTLo8rBM52RxpTt@cluster0.qzeh71x.mongodb.net/test")],
+  imports: [PokemonModule, JoiPipeModule, MongooseModule.forRoot("mongodb+srv://Bianca999:vOTLo8rBM52RxpTt@cluster0.qzeh71x.mongodb.net/test")],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
--- src/pokemon/dto/dtoCreatePokemon.ts
+++ src/pokemon/dto/dtoCreatePokemon.ts
@@ -1,8 +1,26 @@
+import { CREATE, JoiSchema, JoiSchemaOptions, UPDATE } from "nestjs-joi";
+import * as Joi from 'joi';
+
+/**
+ * @description added Joi validation on incoming data
+ */
+@JoiSchemaOptions({
+    allowUnknown: false,
+})
+
 /**
  * @description Data Transfer Object (Dto) Schema for Pokemon (payload)
  */
 export class dtoCreatePokemon {
-    id: number;
-    name: string;
-    image: string;
+    @JoiSchema(Joi.number().required())
+    @JoiSchema([CREATE], Joi.number().required())
+    id!: number;
+    
+    @JoiSchema(Joi.string().required())
+    @JoiSchema([CREATE], Joi.string().required())
+    name!: string;
+
+    @JoiSchema(Joi.string().required())
+    @JoiSchema([CREATE], Joi.string().required())
+    image!: string;
 }
