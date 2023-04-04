d567b91f212b5c1de687a138d45878a15f58e822
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-04-04 09:19:31 +0100

    modified README file

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -2,7 +2,7 @@ import { Injectable} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import axios from 'axios';
 import { Model } from 'mongoose';
-import { Pokemon, PokemonDocument } from './Pokemon.schema';
+import { Pokemon, PokemonDocument } from './pokemon.schema';
 import { dtoCreatePokemon } from './dto/dtoCreatePokemon';
 
 @Injectable()
